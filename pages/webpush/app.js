aa460928a83c1a29fd81d8397f7789f34672b26d
Rayan Kanso <rayankans@gmail.com>
2018-12-03 16:59:21 +0200

    Setup `webpush` branch

--- pages/webpush/app.js
+++ pages/webpush/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router(); // eslint-disable-line new-cap
 
-router.get('/', (req, res) => res.send('Web Push'));
+router.get('/', (req, res) => res.render(`${__dirname}/index.pug`));
 
 module.exports = router;

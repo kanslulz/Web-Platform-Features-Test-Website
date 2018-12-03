cb927f1d5b8b9c16493b0aa62f9107c55ac6d1a1
Rayan Kanso <rayankans@gmail.com>
2018-12-03 21:10:34 +0200

    register SW and request notif permission

--- pages/webpush/static/js/index.js
+++ pages/webpush/static/js/index.js
@@ -1,8 +1,33 @@
 import {create as createSpinnerController} from './spinner.js';
-  
+
 window.addEventListener('load', setup);
 
-function setup() {
+// Useful globals.
+let serviceWorkerReg = null;
+
+/**
+ * Initial page setup.
+ */
+async function setup() {
+  // Install Service Worker.
+  try {
+    // TODO(kanslulz): Add a scope.
+    serviceWorkerReg =
+        await navigator.serviceWorker.register('static/js/sw.js');
+    document.getElementById('sw-status').textContent = ' ✔️';
+  } catch (e) {
+    document.getElementById('sw-status').textContent = ' ✖️';
+  }
+
+  // Check Notifications Permission.
+  Notification.requestPermission(() => {
+    if (Notification.permission !== 'granted') {
+      document.getElementById('notif-perm').textContent = ' ✖️';
+    } else {
+      document.getElementById('notif-perm').textContent = ' ✔️';
+    }
+  });
+
   const spinnerController = createSpinnerController('spinner');
   spinnerController.hide(2000);
 }

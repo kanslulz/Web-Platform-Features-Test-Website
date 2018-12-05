b719ce1dc4e5462bd65999174b91a6431331b43c
Rayan Kanso <rayankans@gmail.com>
2018-12-05 01:07:51 +0000

    create SW for backgroundsync

--- backgroundsync/static/js/index.js
+++ backgroundsync/static/js/index.js
@@ -1,1 +1,21 @@
-console.log('!!!!!!!');
+window.addEventListener('load', setup);
+
+let serviceWorkerReg = null;
+
+/**
+ * Initial page setup.
+ */
+async function setup() {
+  let prerequisites = true;
+  // Install Service Worker.
+  try {
+    // TODO(kanslulz): Add a scope.
+    serviceWorkerReg =
+        await navigator.serviceWorker.register('static/js/sw.js');
+  } catch (e) {
+    console.log('Failed to register');
+  }
+
+  window.addEventListener('message', event => console.log(event.data));
+  serviceWorkerReg.sync.register('lel');
+}
---
+++ backgroundsync/static/js/sw.js
@@ -0,0 +1,19 @@
+
+self.addEventListener('install', event => {
+  event.waitUntil(skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+async function postMessageToWindow(msg) {
+  const matchedClients = await clients.matchAll({type: 'window'});
+  for (const client of matchedClients) {
+    client.postMessage(msg);
+  }
+}
+
+self.addEventListener('sync', event => {
+  event.waitUntil(postMessageToWindow('Got sync event: ' + event.tag));
+});

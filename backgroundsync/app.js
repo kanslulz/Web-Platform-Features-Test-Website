2a6e97a60687c50f48792eadc31f80878cd12dd9
Rayan Kanso <rayankans@gmail.com>
2018-12-05 00:20:14 +0000

    add grid structure for backgroundsync page

--- backgroundsync/app.js
+++ backgroundsync/app.js
@@ -3,4 +3,9 @@ const router = express.Router(); // eslint-disable-line new-cap
 
 router.get('/', (req, res) => res.render(`${__dirname}/index.pug`));
 
+// TODO(kanslulz): Find an easier way to do static file serving when routing.
+router.use(
+  '/static',
+  (req, res) => res.sendFile(`${__dirname}/static${req.path}`));
+
 module.exports = router;
